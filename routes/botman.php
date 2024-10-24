cd5b7c521a2ca89d6b6b6bd0d2891aedcd70c9d6
wali-eldin-hassan <creaspo6@gmail.com>
2024-10-24 20:17:15 +0300

    added mulit type for replaying

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
+$botman->hears('Hi|Hello|مرحبا|السلام عليكم', function ($bot) {
     $bot->reply('Hello!');
 });
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+//$botman->hears('Start conversation', BotManController::class.'@startConversation');
