4567e3117eb9b0b8812cc5d366dbafa458ea4c76
wali-eldin-hassan <creaspo6@gmail.com>
2024-10-24 20:58:12 +0300

    added fetch weather base on locations

--- routes/botman.php
+++ routes/botman.php
@@ -1,9 +1,11 @@
 <?php
-use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi|Hello|مرحبا|السلام عليكم', function ($bot) {
-    $bot->reply('Hello!');
+$botman->hears('weather in {location}', function ($bot, $location) {
+    $weather = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q='.$location.'&appid=3772f1f9a8f0f22e8ce65ca9535cad3f');
+    $weather = json_decode($weather, true);
+    $bot->reply('The weather in ' . $location .' is currently '. $weather['weather'][0]['description']. '. The temperature is '. $weather['main']['temp']. '°C.');
+    $bot->reply('you entered : ' . $location);
 });
 //$botman->hears('Start conversation', BotManController::class.'@startConversation');
